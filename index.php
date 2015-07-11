c59185cc115a56639f374111b0d6f634907955ff
Mukund Bhudia <mukund_bhudia@hotmail.com>
2015-07-11 19:52:42 +0100

    Created basic search functionality to find an article in elastic search.

--- index.php
+++ index.php
@@ -2,6 +2,28 @@
 
 require_once 'app/init.php';
 
+if (isset($_GET['q'])) {
+    $q = $_GET['q'];
+
+    $query = $es->search([
+        'body' => [
+            'query' => [
+                'bool' => [
+                    'should' => [
+                        'match' => ['title' => $q],
+                        'match' => ['body' => $q],
+                        'match' => ['keywords' => $q]
+                    ]
+                ]
+            ]
+        ]
+    ]);
+
+    if ($query['hits']['total'] >= 1) {
+        $results = $query['hits']['hits'];
+    }
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -75,6 +97,26 @@ require_once 'app/init.php';
                 </form>
             </div>
         </div>
+
+        <div class="row">
+            <?php
+            if (isset($results)) {
+                foreach ($results as $r) {
+                    ?>
+                        <div class="result">
+                            <a href="#<?php echo $r['_id']; ?>">
+                                <?php echo $r['_source']['title']; ?>
+                            </a>
+                            <div>
+                                <?php echo implode(', ', $r['_source']['keywords']); ?>
+                            </div>
+                        </div>
+                    <?php
+                }
+            }
+            ?>
+        </div>
+
     </div> <!-- /container -->
 
 
